6fa81ea7e2c86efb218a3437d7148eea345d0f83
RossLitz <codingclasses.049@gmail.com>
2015-04-27 12:56:09 -0500

    Searching JSON data

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-$.getJSON('data.json', function(data){
-	var output = '<ul class= "searchresults">';
-	$.each(data, function(key,val){
-		output += '<li>';
-		output +=	'<h2>' + val.name + '</h2>';
-		output +=	'<image src="images/' + val.shortname + '_tn" alt="' + val.name + '" />';
-		output +=	'<p>' + val.bio + '</p>';
- 		output += '</li>';
-	});
-	output += '</ul>';
-	$('#update').html(output);
-});//get JSON
+$('#search').keydown(function() {
+	var searchField = $('#search').val();
+	var myExp = new RegExp(searchField, "i");
+
+	$.getJSON('data.json', function(data){
+		var output = '<ul class= "searchresults">';
+		$.each(data, function(key,val){
+			//searching name or bio 
+			if((val.name.search(myExp) !=  -1) || (val.bio.search(myExp) != -1) ) {
+				output += '<li>';
+				output +=	'<h2>' + val.name + '</h2>';
+				output +=	'<image src="images/' + val.shortname + '_tn" alt="' + val.name + '" />';
+				output +=	'<p>' + val.bio + '</p>';
+		 		output += '</li>';
+		 	};
+		});
+		output += '</ul>';
+		$('#update').html(output);
+	});//get JSON
+});
